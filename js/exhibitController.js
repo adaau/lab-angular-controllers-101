32564ff26e2c3cb20e52b41be86fe826f5077dd4
adaau <ada.au.cheng@gmail.com>
2015-11-30 15:09:32 +0800

    image working

--- js/exhibitController.js
+++ js/exhibitController.js
@@ -2,7 +2,8 @@ angular.module('MuseumApp')
   .controller('ExhibitController', ExhibitController);
 
 function ExhibitController($scope) {
-  $scope.image = "../marilyn.jpg";
+  // $scope.image = "../marilyn.jpg";
+  $scope.image = "http://www.warhol.org/exhibitions/2012/15minuteseternal/en/img/art-marilyn-470x469.jpg";
   $scope.name = "Pop Art";
   $scope.artist = "Andy Warhol"
 }
