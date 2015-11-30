d025736b4a4c5b988e7e9e7b5a4c68adf2106d95
adaau <ada.au.cheng@gmail.com>
2015-11-30 15:12:54 +0800

    changed donations to not use ng-repeat

--- js/donorController.js
+++ js/donorController.js
@@ -4,5 +4,12 @@ angular.module('MuseumApp')
 function DonorController($scope) {
   $scope.email = 'development@momamuseum.org';
   $scope.phone = '212-123-4567';
-  $scope.amounts = [50, 100, 250, 500, 1000, 5000];
+  $scope.amount1 = 50;
+  $scope.amount2 = 100;
+  $scope.amount3 = 250;
+  $scope.amount4 = 500;
+  $scope.amount5 = 1000;
+  $scope.amount6 = 5000;
+
+  // $scope.amounts = [50, 100, 250, 500, 1000, 5000];
 }
